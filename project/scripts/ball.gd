d29286545664ff3c9032a7427453aaa018af4c04
wherefore <wherefore0@gmail.com>
2025-11-27 13:24:39 -0500

    more robust ball bouncing
    instead of simple 45, options for multiplying y velocity by distance from paddle center on collision:
    -on/off
    -multiplying factor
    -whether to affect y with regular acceleration (since this system already multiplies the y frequently)

--- project/scripts/ball.gd
+++ project/scripts/ball.gd
@@ -1,11 +1,24 @@
 extends Area2D
 
 signal destroyed
+var velocity = Vector2.ZERO #instantiates
 
+#base ball settings
 var ball_speed = 600
-var velocity = Vector2.ZERO
-var acceleration_factor = 1.12 #to give the game a ramping time limit
-var random_starting_direction_tilt = 0.3
+var random_starting_direction_tilt = 0.3 # how far from horizontal to start the ball moving
+
+#acceleration on paddle bounce
+var acceleration_factor = 1.08 #to give the game a ramping time limit
+var accelerate_x = true
+var accelerate_y = false
+
+#rebound at non-45 on paddle bounce: as a factor of distance to center of paddle on collision
+var use_angle_rebound = true
+var rebound_angle_factor = 0.5
+#two purposes: bandaid to solve the ball center issue, and could be used to limit the power of edge bounces
+var clamp_rebound_edges = true
+var clamp_rebound_edge_max = 0.85 
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -32,17 +45,34 @@ func _process(delta: float) -> void:
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("Paddle"):
-		velocity *= acceleration_factor
-		velocity *= Vector2(-1,1)
+		# known issue: this math isn't perfectly accurate since i'm not measuring where the ball impacted the paddle
+		var ball_pos = position.y #this is the center of the ball, not the position of the collision
+		var paddle_pos = area.position.y
+		var distance_to_paddle_center = abs(ball_pos - paddle_pos)
+		var paddle_length_to_center = area.get_node("CollisionShape2D").shape.height/2
+		var percent_of_paddle_from_center = distance_to_paddle_center / paddle_length_to_center
+		
+		if clamp_rebound_edges: 
+			percent_of_paddle_from_center = min(percent_of_paddle_from_center, clamp_rebound_edge_max)
+
+		if use_angle_rebound:
+			velocity *= (Vector2(1, 1+(percent_of_paddle_from_center*rebound_angle_factor)))
+		if accelerate_x:
+			velocity.x *= acceleration_factor
+		if accelerate_y:
+			velocity.y *= acceleration_factor
+
+		velocity *= Vector2(-1,1) #basic reverse direction
 		
 	elif area.is_in_group("Bounce Wall"):
 		velocity *= Vector2(1,-1)
 	
 	#failsafe: checks if it has collided with something recently
-	#if not it's probably out of bounds, so yeet
+	#restarts timer when colliding with anything
 	$Timer.start() #restart timer
 
-
+#failsafe: checks if it has collided with something recently
+#if it hasn't collided in a while, yeet
 func _on_timer_timeout() -> void:
 	emit_signal("destroyed")
 	queue_free()
