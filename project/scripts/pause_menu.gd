33b3e65062344e80bf793d64cae7bcf90903d132
wherefore <wherefore0@gmail.com>
2025-11-28 14:24:17 -0500

    ui stuff
    fonts, pause menu

---
+++ project/scripts/pause_menu.gd
@@ -0,0 +1,36 @@
+extends CanvasLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("main menu") and not get_tree().paused:
+		pause()
+	elif Input.is_action_just_pressed("main menu") and get_tree().paused:
+		unpause()
+
+func pause():
+	get_tree().paused = true
+	
+	$MenuBody.modulate.a = 0
+	var tween = create_tween()
+	tween.set_trans(Tween.TRANS_EXPO)
+	tween.set_ease(Tween.EASE_OUT)
+	tween.tween_property($MenuBody, "modulate", Color(1,1,1,1), 0.3)
+	visible = true
+		
+		
+func unpause():
+	$MenuBody.modulate.a = 1
+	var tween = create_tween()
+	tween.set_trans(Tween.TRANS_EXPO)
+	tween.set_ease(Tween.EASE_OUT)
+	tween.tween_property($MenuBody, "modulate", Color(1,1,1,0), 0.4)
+	
+	await tween.finished
+	
+	get_tree().paused = false
+	visible = false
