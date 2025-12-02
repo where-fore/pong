bbf36889df1a93bc7c2f3cf85eb31f77cc14f514
wherefore <wherefore0@gmail.com>
2025-12-02 15:08:11 -0500

    some restructuring
    also tuned the paddle mouse deadzone

--- project/scripts/paddle.gd
+++ project/scripts/paddle.gd
@@ -1,38 +1,32 @@
 extends Area2D
 
 var paddle_speed_factor = 500
-var screen_size = 0
-var paddle_type = "Keys"
+var screen_size = 0 # init at ready
 var paddle_disabled_on_collision_for = 0.2
+var velocity = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	screen_size = get_viewport_rect().size
-	if is_in_group("Cursor Paddle"):
-		paddle_type= "Cursor"
+	screen_size = get_viewport_rect().size #going to use later for clamping
+	
+	
+	if is_in_group("Right Paddle"):
 		$AnimatedSprite2D.flip_v = true #because the node in scene is just rotated 180 degrees
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var velocity = Vector2.ZERO
+	velocity = Vector2.ZERO #clear old velocity
 	
-	if paddle_type == "Keys":
-		if Input.is_action_pressed("move_up"):
-			velocity.y = -1
-		if Input.is_action_pressed("move_down"):
-			velocity.y = 1
+	if is_in_group("Keys Paddle"):
+		key_movement()
 		
-	if paddle_type == "Cursor":
-		var mouse_position = get_global_mouse_position()
-		var paddle_position = position
-		var mouse_difference_tolerance = 20
-		if mouse_position.y - paddle_position.y > mouse_difference_tolerance:
-			velocity.y = 1
-		elif paddle_position.y - mouse_position.y > mouse_difference_tolerance:
-			velocity.y = -1
-		
-		
+	if is_in_group("Cursor Paddle"):
+		cursor_movement()
+			
+	if is_in_group("AI Paddle"):
+		ai_movement()
+
 	if velocity.y < 0:
 		$AnimatedSprite2D.play("up")
 	elif velocity.y > 0:
@@ -50,3 +44,22 @@ func _on_area_entered(area: Area2D) -> void:
 		$CollisionShape2D.disabled = true
 		await get_tree().create_timer(paddle_disabled_on_collision_for).timeout
 		$CollisionShape2D.disabled = false
+
+func key_movement():
+	if Input.is_action_pressed("move_up"):
+		velocity.y = -1
+	if Input.is_action_pressed("move_down"):
+		velocity.y = 1
+
+func cursor_movement():
+	var mouse_position = get_global_mouse_position()
+	var paddle_position = position
+	var mouse_difference_tolerance = 10
+	
+	if mouse_position.y - paddle_position.y > mouse_difference_tolerance:
+		velocity.y = 1
+	elif paddle_position.y - mouse_position.y > mouse_difference_tolerance:
+		velocity.y = -1
+		
+func ai_movement():
+	pass
