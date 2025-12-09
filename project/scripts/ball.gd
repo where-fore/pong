c465c5c6f8a6189eaf8042a8f2efeb3309795d07
wherefore <wherefore0@gmail.com>
2025-12-08 21:13:04 -0500

    multiball is back in
    actually i don't think it has any downsides, you can fix it all be going back to main menu really easily

--- project/scripts/ball.gd
+++ project/scripts/ball.gd
@@ -89,7 +89,8 @@ func _on_timer_timeout() -> void:
 
 func drop_in():
 	#so the ball doesn't score or something if it grows near an edge
-	$CollisionShape2D.disabled = true
+	#$CollisionShape2D.disabled = true
+	#actually multiball has no downsides for now
 	
 	#"self" to scale the parent: the sprite and all colliders
 	#"$Sprite2D" to scale the sprite only: doesn't mess with colliders
@@ -137,7 +138,8 @@ func finish_drop_in_tween():
 		drop_in_tween = null
 		
 		#turn collision back on, of course
-		$CollisionShape2D.disabled = false
+		#$CollisionShape2D.disabled = false
+		#actually multiball has no downsides for now
 		
 		#return the ball to its original speed
 		velocity = original_velocity
